8713d4493035b020b769a63e5c9ec2abd925b32b
Ethan-shadow <97447053+Ethan-shadow@users.noreply.github.com>
2024-12-02 00:04:05 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
-require('dotenv').config()
-const { Client } = require('discord.js-selfbot-v13')
-const client = new Client()
-
-client.on('ready', async () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-
-    const channel = await client.channels.fetch(process.env.BUMP_CHANNEL)
-    
-    async function bump() {
-        await channel.sendSlash('302050872383242240', 'bump')
-        console.count('Bumped!')
-    }
-
-    function loop() {
-        // send bump message every 2-3 hours, to prevent detection.
-        var randomNum = Math.round(Math.random() * (9000000 - 7200000 + 1)) + 7200000
-        setTimeout(function () {
-            bump()
-            loop()
-        }, randomNum)
-    }
-    
-    bump()
-    loop()
-})
-
-client.login(process.env.TOKEN)
+require('dotenv').config();
+const { Client } = require('discord.js-selfbot-v13');
 const express = require('express');
 const app = express();
 
@@ -34,11 +8,8 @@ const port = process.env.PORT || 3000; // Render will provide the PORT environme
 app.get('/', (req, res) => res.send('Bot is running!')); // Simple route to ensure the app is responsive
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
 
-require('dotenv').config();
-const { Client } = require('discord.js-selfbot-v13');
 const client = new Client();
 
-// Your existing bumping logic goes here
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
